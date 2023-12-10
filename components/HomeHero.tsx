304fa0dae175982bb5a2ce8dcd24bd22bf786169
Bryan Diaz <bryandiaz@Bryans-MacBook-Pro.local>
2023-12-10 16:53:15 -0500

    Use Git LFS for large files

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -1,8 +1,9 @@
-// components/Hero.tsx
+// components/HomeHero.tsx
 import React from 'react';
 import styles from './HomeHero.module.css';
+import { useRouter } from 'next/router';
 
-const Hero: React.FC = () => {
+const HomeHero: React.FC = () => {
   const handleScroll = () => {
     const section = document.getElementById('scroll-target'); // Replace 'scroll-target' with the ID of your target section
     if (section) {
@@ -10,16 +11,35 @@ const Hero: React.FC = () => {
     }
   };
 
+  const router = useRouter();
+
+  const routeToContact = () => {
+    router.push('/contact');
+  };
+
   return (
     <div className={styles.hero}>
       <div className={styles.overlay}></div>
       <div className={styles.content}>
-        <h1 style={{color: 'white'}}>Twin Pines Landscaping</h1>
-        <p style={{color: 'white'}}>Transforming Outdoor Spaces with Excellence</p>
-        <button onClick={handleScroll}>View our services</button>
+        <h1 style={{ color: 'white' }}>Twin Pines Landscaping</h1>
+        <p style={{ color: 'white' }}>Transforming Outdoor Spaces with Excellence</p>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100vw', alignItems: 'center', flexDirection: 'column' }}>
+          <button style={{ marginBottom: '2rem' }} onClick={handleScroll}>
+            View our services
+          </button>
+          <div style={{ position: 'relative', cursor: 'pointer' }} onClick={handleScroll}>
+            <div style={{ width: '25px', height: '25px', border: 'solid white', borderWidth: '0 3px 3px 0', display: 'inline-block', padding: '3px', transform: 'rotate(45deg)' }}></div>
+          </div>
+          <p style={{ marginTop: '1rem', color: 'white', cursor: 'pointer' }} onClick={handleScroll}>
+            Learn More
+          </p>
+          <button style={{ marginLeft: '2rem' }} onClick={routeToContact}>
+            Contact us
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Hero;
+export default HomeHero;
